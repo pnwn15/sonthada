2712c8cf2b1af2602da2d6a5f56be6c8e11c7fb9
pnwn15 <panawatzaa2544@gmail.com>
2024-06-03 09:29:37 +0700

    fah

---
+++ src/views/signup.vue
@@ -0,0 +1,66 @@
+<template>
+<div class="bg-[#F4EFDF] w-full h-full flex fixed items-start">
+   <form class="max-w-sm mx-auto ">
+    <div class="flex justify-center text-4xl font-bold mt-14">
+    <h1>สร้างบัญชีผู้ใช้ใหม่</h1>
+    </div>
+  <div class="mt-8">
+    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">อีเมล</label>
+    <input type="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
+  </div>
+  <div class="mt-2">
+    <label  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">รหัสผ่าน</label>
+    <input type="password"  class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+  </div>
+  <div class="mt-2">
+    <label  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ยืนยันรหัสผ่าน</label>
+    <input type="password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+  </div>
+  <div class="mt-2">
+    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">เบอร์โทรศัพท์</label>
+    <input type="text"  class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+  </div>
+  <div class="mt-2">
+    <label  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ที่อยู่ปัจจุบัน</label>
+    <input type="text"  class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+  </div>
+  <div class="flex items-start mt-2">
+    <div class="flex items-center h-5">
+      <input id="terms" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+    </div>
+    <label for="terms" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" class="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a></label>
+  </div>
+  <button type="submit"  @click="openDialog" class="text-white bg-[#771011]  mt-5 w-full focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">สมัครสมาชิก</button>
+</form>
+  </div>
+<div v-if="dialogVisible" class="dialog-overlay">
+      <div class="dialog-content">
+        <span @click="closeDialog" class="close-btn">&times;</span>
+        <h2>Dialog Title</h2>
+        <p>Dialog content goes here...</p>
+      </div>
+    </div>
+  
+</template>
+
+
+
+<script>
+export default {
+    name:"signup",
+    data() {
+    return {
+      dialogVisible: false
+    }
+  },
+  methods: {
+    openDialog() {
+      this.dialogVisible = true;
+    },
+    closeDialog() {
+      this.dialogVisible = false;
+    }
+  }
+}
+</script>
+
