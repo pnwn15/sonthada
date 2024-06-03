61063e5bcdadc3289528fcf333c96b1ee704e889
pnwn15 <panawatzaa2544@gmail.com>
2024-06-03 21:14:36 +0700

    ส่ง

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
 <navbar></navbar>
+<footer></footer>
 <router-view></router-view>
 </template>
 
 <script>
 
  import navbar from '/src/components/navbar.vue'
+ import footer from '/src/components/footer.vue'
 export default {
   components: {
-    navbar
+    navbar,
+    footer
   }
 }
 </script>
