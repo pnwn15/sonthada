cc3acf00f9cb92d34aeebe6dc11934f73ba65e15
pnwn15 <panawatzaa2544@gmail.com>
2024-06-04 22:10:48 +0700

    ส่งงาน

--- src/router/index.ts
+++ src/router/index.ts
@@ -26,10 +26,7 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/about.vue')
+      component: about
     }
   ]
 })
